0c24a191856abcf817dd7819f2c4ffd951e86192
Anton Berezin <gurunars@gmail.com>
2020-09-05 21:54:49 +0200

    ff

--- src/functionArgValidation.ts
+++ src/functionArgValidation.ts
@@ -3,20 +3,19 @@ import { numberField, stringField } from "./fields";
 
 export const tuple = <T extends any[]>(...args: T): T => args
 
-export const withValidation = <T extends any[], R> (spec: ValidatorSpec<T>, rawCall: (...T) => R): (value: any) => R => 
+export const withValidation = <T extends any[], R> (spec: ValidatorSpec<T>, rawCall: (...values: T) => R): (value: any) => R => 
     (value: any): R => rawCall(...validate<T>(spec, value))
 
-const test = () => {
 
-    // TODO: how to force a list to be interpreted as a
-    const spec = tuple(
-        stringField(),
-        stringField(),
-        numberField(),
-    )
+// TODO: how to force a list to be interpreted as a
+const spec = tuple(
+    stringField(),
+    stringField(),
+    numberField(),
+)
 
-    const someFunction = (a: string, b: string, c: number) => `${a}${b}${c}`
+const someFunction = (a: string, b: string, c: number) => `${a}${b}${c}`
 
-    const validated = withValidation(spec, someFunction)
+const wildCard = withValidation(spec, someFunction)
 
-}
+wildCard('a', '')
