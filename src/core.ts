6835b1405b4815f01c292417426fa482cd457026
Anton Berezin <gurunars@gmail.com>
2020-09-05 19:24:42 +0200

    ff

--- src/core.ts
+++ src/core.ts
@@ -12,18 +12,17 @@ class GetParams {}
 
 const GET_PARAMS = new GetParams();
 
-export const getParams = <T extends {}> (validatorSpec: ValidatorSpec<T>): any => Object.fromEntries(
+const mapSpec = <T extends {}, R> (validatorSpec: ValidatorSpec<T>, transform: (validator: ValidatorFunction<T>, key: string) => R): any => Object.fromEntries(
     Object.entries(validatorSpec).map(
-        ([key, validator]: [string, any]) => [key, validator(GET_PARAMS)]
+        ([key, validator]: [string, any]) => [key, transform(validator, key)]
     )
 );
+
+export const getParams = <T extends {}> (validatorSpec: ValidatorSpec<T>): any => 
+    mapSpec(validatorSpec, validator => validator(GET_PARAMS));
     
-export const validate = <T extends {}> (validatorSpec: ValidatorSpec<T>, value: any): T => 
-    Object.fromEntries(
-        Object.entries(validatorSpec).map(
-            ([key, validator]: [string, any]) => [key, validator(value[key])]
-        )
-    ) as T;
+export const validate = <T extends {}> (validatorSpec: ValidatorSpec<T>, value: any): T =>
+    mapSpec(validatorSpec, (validator, key) => validator(value[key]));
 
 export type ValidatorFunctionWithSpec<Params, T> = (params: Params, value: any) => any
 
