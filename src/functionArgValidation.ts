6b5862555d2d0417f711c67fcdb9a3a8fc6cf187
Anton Berezin <gurunars@gmail.com>
2020-09-05 22:08:18 +0200

    ff

--- src/functionArgValidation.ts
+++ src/functionArgValidation.ts
@@ -6,10 +6,11 @@ export const tuple = <T extends any[]>(...args: T): T => args
 export const withValidation = <T extends any[], R> (spec: ValidatorSpec<T>, rawCall: (...values: T) => R) => 
     (...value: any[]): R => rawCall(...validate<T>(spec, value))
 
-const wildCard = withValidation(tuple(
-    stringField(),
-    stringField(),
-    numberField(),
-), (a, b, c) => `${a}${b}${c}`)
-
-console.log(wildCard('a', 'b', 1))
+const test = () => {
+    const wildCard = withValidation(tuple(
+        stringField(),
+        stringField(),
+        numberField(),
+    ), (a, b, c) => `${a}${b}${c}`)
+    console.log(wildCard('a', 'b', 1))
+}
