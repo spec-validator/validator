b09c7eb3f7d0e5a1b8b975586eaff8d134f13d0f
Anton Berezin <__EMAIL>
2020-09-07 17:23:51 +0200

    ff

--- src/fields/arrayField.ts
+++ src/fields/arrayField.ts
@@ -1,13 +1,17 @@
-import { Field, declareField, Mode } from '../core';
+import { Field, declareField, Mode, withErrorDecoration } from '../core';
 
 const arrayField = <T> (itemField: Field<T>): Field<T[]> => declareField({
   validate: (value: any): T[] => {
     if (!Array.isArray(value)) {
       throw 'Not an array'
     }
-    return value.map((it) => itemField(it, Mode.VALIDATE));
+    return value.map(
+      (it, index) => withErrorDecoration(index, () => itemField(it, Mode.VALIDATE))
+    );
   },
-  serialize: (value: T[]) => value.map((it) => itemField(it, Mode.SERIALIZE)),
+  serialize: (value: T[]) => value.map(
+    (it, index) => withErrorDecoration(index, () => itemField(it, Mode.SERIALIZE))
+  ),
   getParams: () => Object.assign({item: itemField(undefined, Mode.GET_PARAMS)}),
 })
 
