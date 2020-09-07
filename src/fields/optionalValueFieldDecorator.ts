9e508e303687e8f55c47fd5c8261c741f93d8877
Anton Berezin <__EMAIL>
2020-09-08 01:35:03 +0200

    ff

---
+++ src/fields/optionalValueFieldDecorator.ts
@@ -0,0 +1,15 @@
+import { Field, declareField, Mode } from '../core';
+import { merge, Optional } from '../utils';
+
+const optionalField = <T> (innerField: Field<T>): Field<Optional<T>> => declareField({
+  validate: (value: any): Optional<T> => {
+    if (value === undefined) {
+      return value
+    }
+    return innerField(value, Mode.VALIDATE)
+  },
+  serialize: (value: Optional<T>) => value === undefined ? value : innerField(value, Mode.SERIALIZE),
+  getParams: () => merge(innerField(undefined, Mode.GET_PARAMS), {isOptional: true}),
+})
+
+export default optionalField;
