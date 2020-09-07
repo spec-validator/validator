f5d0096a6148fba3fbdaada6ca72b58b745689ee
Anton Berezin <__EMAIL>
2020-09-08 01:32:11 +0200

    ff

--- src/fields/booleanField.ts
+++ src/fields/booleanField.ts
@@ -1,10 +1,13 @@
 import { declareParametrizedField } from '../core'
+import { optionalOf } from '../utils';
 
 const booleanField = declareParametrizedField({
-  defaultParams: {},
-  validate: (_, value: any): number => {
-    if (typeof value !== 'number') {
-      throw 'Not a number'
+  defaultParams: {
+    description: optionalOf<string>()
+  },
+  validate: (_, value: any): boolean => {
+    if (value !== true && value !== false) {
+      throw 'Not a boolean'
     }
     return value
   }
