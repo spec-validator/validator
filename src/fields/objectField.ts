888b27cbdb43fa77da0611472d6e801c1dd85210
Anton Berezin <__EMAIL>
2020-09-08 00:56:32 +0200

    ff

--- src/fields/objectField.ts
+++ src/fields/objectField.ts
@@ -1,14 +1,22 @@
-import { Field, declareField, validate, ValidatorSpec } from '../core';
+import { serialize } from 'v8';
+import {
+  Field,
+  declareField,
+  ValidatorSpec,
+  validate as rawValidate,
+  serialize as rawSerialize,
+  getParams as rawGetParams
+} from '../core';
 
 const objectField = <ExpectedType> (objectSpec: ValidatorSpec<ExpectedType>): Field<ExpectedType> => declareField({
   validate: (value: any): ExpectedType => {
     if (typeof value !== 'object') {
       throw 'Not an object'
     }
-    return validate(objectSpec, value)
+    return rawValidate(objectSpec, value)
   },
-  serialize: (value: any) => {},
-  getParams: () => {}
+  serialize: (value: ExpectedType) => rawSerialize(objectSpec, value),
+  getParams: () => rawGetParams(objectSpec)
 })
 
 export default objectField;
