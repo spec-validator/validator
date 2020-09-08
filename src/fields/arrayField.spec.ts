1c079e8798503f166a054c9f8b64751030d439a9
Anton Berezin <gurunars@gmail.com>
2020-09-08 23:23:08 +0200

    ff

--- src/fields/arrayField.spec.ts
+++ src/fields/arrayField.spec.ts
@@ -2,7 +2,7 @@ import { arrayField } from '.';
 import { TypeHint, validate } from '../core';
 import numberField from './numberField';
 
-test('allows valid array to get throw', () => {
+test('allows valid array to get through', () => {
   const spec = {
     field: arrayField({itemField: numberField()}),
   }
@@ -11,4 +11,17 @@ test('allows valid array to get throw', () => {
     field: [1]
   });
   expect(valid.field).toEqual([1]);
+});
+
+test('reports an error with full path leading to it if there are issues', () => {
+  const spec = {
+    field: arrayField({itemField: numberField()}),
+  }
+  try {
+    validate(spec, {
+      field: [1, 2, false]
+    });
+  } catch (err) {
+    expect(err).toEqual({'inner': 'Not a number', 'path': ['field', 2]})
+  }
 });
