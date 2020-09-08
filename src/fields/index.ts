49c56714fbb977e2cbd70ed84df164d9878293f7
Anton Berezin <__EMAIL>
2020-09-08 10:01:07 +0200

    ff

---
+++ src/fields/index.ts
@@ -0,0 +1,18 @@
+import arrayField from './arrayField';
+import booleanField from './booleanField';
+import numberField from './numberField';
+import objectField from './objectField';
+import stringField from './stringField';
+
+import defaultValueDecor from './defaultValueDecor';
+import optionalValueDecor from './optionalValueDecor';
+
+export {
+  arrayField,
+  booleanField,
+  numberField,
+  objectField,
+  stringField,
+  optionalValueDecor,
+  defaultValueDecor
+}
