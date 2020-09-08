da7ca26c1d395cbc059e4e9192628d6c35d1b45c
Anton Berezin <__EMAIL>
2020-09-08 11:07:00 +0200

    ff

--- src/fields/choiceField.spec.ts
+++ src/fields/choiceField.spec.ts
@@ -3,7 +3,7 @@ import { TypeHint, validate } from '../core';
 
 test('choiceField validation prevents invalid choices from getting through', () => {
   const spec = {
-    field: choiceField({choices: [1, 2, 3] as const})
+    field: choiceField({choices: [1, 2, 3] as const}),
   }
   type Spec = TypeHint<typeof spec>
   const valid: Spec = validate(spec, {
