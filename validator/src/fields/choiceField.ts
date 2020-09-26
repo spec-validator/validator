803cc9efa1e53ee3ebc656dfc565d300fd2d25c6
Anton Berezin <gurunars@gmail.com>
2020-09-26 21:18:14 +0200

    ff

--- validator/src/fields/choiceField.ts
+++ validator/src/fields/choiceField.ts
@@ -9,6 +9,7 @@ type Params<Choices> = {
 class ChoiceField<Choices extends readonly Primitive[], T=Choices[number]> implements Field<T>, WithRegExp {
   private params: Params<Choices>
   private choicesSet: Set<Primitive>
+  private stringChoiceMap: Record<string, Primitive>
 
   constructor(params: {
     choices: Choices,
@@ -16,14 +17,19 @@ class ChoiceField<Choices extends readonly Primitive[], T=Choices[number]> imple
   }) {
     this.params = params
     this.choicesSet = new Set(params.choices)
+    this.stringChoiceMap = Object.fromEntries(params.choices.map(it => [it.toString(), it]))
   }
   regex: () => RegExp;
 
   validate(value: any): T {
-    if (!this.choicesSet.has(value)) {
-      throw 'Invalid choice'
+    if (this.choicesSet.has(value)) {
+      return value as T
     }
-    return value as T
+    const mapped = this.stringChoiceMap[value];
+    if (mapped !== undefined) {
+      return mapped as any;
+    }
+    throw 'Invalid choice'
   }
   serialize(deserialized: T): Json {
     return deserialized as unknown as Primitive
