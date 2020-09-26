770bf6098285b150c1db564f1f1a658b93408c3e
Anton Berezin <gurunars@gmail.com>
2020-09-26 20:15:32 +0200

    ff

--- validator/src/withValidation.ts
+++ validator/src/withValidation.ts
@@ -1,13 +1,12 @@
-import { Field, Mode, validate, ValidatorSpec } from './core';
+import { Field, validate, ValidatorSpec } from './core';
 
 const withValidation = <T extends any[], R> (
   argSpec: ValidatorSpec<T>,
   returnValueSpec: Field<R>,
   rawCall: (...values: T) => R
 ) =>
-    (...value: any[]): any => returnValueSpec(
+    (...value: any[]): any => returnValueSpec.serialize(
       rawCall(...validate<T>(argSpec, value)),
-      Mode.SERIALIZE
     )
 
 export default withValidation;
