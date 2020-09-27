36e6cbbb1f579480b3c21941df8200eb2f83da91
Anton Berezin <gurunars@gmail.com>
2020-09-28 00:40:34 +0200

    ff

--- packages/rest-api-server/src/server.ts
+++ packages/rest-api-server/src/server.ts
@@ -1,25 +1,37 @@
-import { Json, ValidatorSpec } from '@validator/validator/core';
+import { ValidatorSpec } from '@validator/validator/core';
 import { Segment } from '@validator/validator/segmentChain';
 
 type Method = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
 
-type Request<PathParams, Data> = {
+type Request<PathParams, Data, QueryParams, Headers> = {
   pathParams: PathParams,
-  data: Data
+  queryParams: QueryParams
+  data: Data,
+  headers: Headers
 }
 
-type Response<Data> = {
-  data: Data
+type Response<Data, Headers> = {
+  data: Data,
+  headers: Headers
 }
 
 class Server {
 
-  route<PathParams, RequestData, ResponseData>(config: {
+  route<PathParams, RequestData, ResponseData, QueryParams, RequestHeaders, ResponseHeaders>(config: {
     method: Method,
-    path: Segment<PathParams>,
-    requestDataSpec: ValidatorSpec<RequestData>,
-    responseDataSpec: ValidatorSpec<ResponseData>,
-    handler: (request: Request<PathParams, RequestData>) => Response<ResponseData>
+    requestSpec: {
+      path: Segment<PathParams>,
+      data: ValidatorSpec<RequestData>,
+      query: ValidatorSpec<QueryParams>,
+      headers: ValidatorSpec<RequestHeaders>
+    },
+    responseSpec: {
+      data: ValidatorSpec<ResponseData>,
+      headers: ValidatorSpec<ResponseHeaders>
+    }
+    handler: (
+      request: Request<PathParams, RequestData, QueryParams, RequestHeaders>
+    ) => Response<ResponseData, ResponseHeaders>
   }) {
     config
     // TODO
