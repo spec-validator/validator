4124d152d9c6b097eca98e71a0c42b20a0be665f
Anton Berezin <gurunars@gmail.com>
2020-09-28 00:34:05 +0200

    ff

--- packages/rest-api-server/src/server.ts
+++ packages/rest-api-server/src/server.ts
@@ -1,1 +1,29 @@
-import { root } from '@validator/validator/segmentChain';
+import { Json, ValidatorSpec } from '@validator/validator/core';
+import { Segment } from '@validator/validator/segmentChain';
+
+type Method = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
+
+type Request<PathParams, Data> = {
+  pathParams: PathParams,
+  data: Data
+}
+
+type Response<Data> = {
+  data: Data
+}
+
+class Server {
+
+  route<PathParams, RequestData, ResponseData>(config: {
+    method: Method,
+    path: Segment<PathParams>,
+    requestDataSpec: ValidatorSpec<RequestData>,
+    responseDataSpec: ValidatorSpec<ResponseData>,
+    handler: (request: Request<PathParams, RequestData>) => Response<ResponseData>
+  }) {
+    config
+    // TODO
+  }
+
+
+}
