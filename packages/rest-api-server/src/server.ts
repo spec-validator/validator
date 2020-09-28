739b70edad5c9fed4c0366e96e7a83dacb951439
Anton Berezin <gurunars@gmail.com>
2020-09-28 22:17:21 +0200

    ff

--- packages/rest-api-server/src/server.ts
+++ packages/rest-api-server/src/server.ts
@@ -1,8 +1,19 @@
 import { request } from 'http';
 
 import { ValidatorSpec } from '@validator/validator/core';
-import { stringField } from '@validator/validator/fields';
 import { root, Segment } from '@validator/validator/segmentChain';
+
+type UnmatchedRequest = {
+  method: string;
+  path: string;
+  body: any;
+  headers: Record<string, string>
+}
+
+type UnmatchedResponse = {
+  body: any;
+  headers: Record<string, string>
+}
 
 type Request<PathParams=undefined, Data=undefined, QueryParams=undefined, Headers=undefined> = {
   path: string;
@@ -19,16 +30,31 @@ type Response<Data=undefined, Headers=undefined> = {
   headers?: Headers,
 }
 
-type WRequest = Request<any, any, any, any>
-type WResponse = Response<any, any>
-
-type Route = {
+type Route<
+  RequestParams=undefined,
+  RequestData=undefined,
+  RequestQueryParams=undefined,
+  RequestHeaders=undefined,
+  ResponseData=undefined,
+  ResponseHeaders=undefined,
+> = {
   method?: string,
   path: Segment<unknown>,
-  handler: (request: WRequest) => Promise<WResponse>
+  requestSpec?: {
+    data?: ValidatorSpec<RequestData>,
+    query?: ValidatorSpec<RequestQueryParams>,
+    headers?: ValidatorSpec<RequestHeaders>
+  },
+  responseSpec?: {
+    data?: ValidatorSpec<ResponseData>,
+    headers?: ValidatorSpec<ResponseHeaders>
+  }
+  handler: (
+    request: Request<RequestParams, RequestData, RequestQueryParams, RequestHeaders>
+  ) => Promise<Response<ResponseData, ResponseHeaders>>,
 }
 
-const matchRoute = (request: WRequest, route: Route): boolean => {
+const matchRoute = (request: UnmatchedRequest, route: Route): boolean => {
   if (route.method && request.method !== route.method) {
     return false;
   }
@@ -38,79 +64,21 @@ const matchRoute = (request: WRequest, route: Route): boolean => {
     return false;
   }
   return true;
-}
+};
 
-export const handle = (
+const handleRoute = async (route: Route, request: UnmatchedRequest): Promise<UnmatchedResponse> => {
+
+};
+
+const handle = async (
   routes: Route[],
-  request: WRequest
-): Promise<WResponse> => {
-  const route = routes.find(matchRoute.bind(null, request))
+  request: UnmatchedRequest
+): Promise<UnmatchedResponse> => {
+  const route = routes.find(matchRoute.bind(null, request));
   if (!route) {
     return Promise.reject(404);
   }
-
+  return handleRoute(route, request);
 }
 
-class Server {
-  private docsRoute: string;
 
-  constructor(docsRoute='/docs') {
-    this.docsRoute = docsRoute;
-  }
-
-  route<
-    RequestParams=undefined,
-    RequestData=undefined,
-    RequestQueryParams=undefined,
-    RequestHeaders=undefined,
-    ResponseData=undefined,
-    ResponseHeaders=undefined,
-  >(config: {
-    method: string,
-    path: Segment<RequestParams>,
-    requestSpec?: {
-      data?: ValidatorSpec<RequestData>,
-      query?: ValidatorSpec<RequestQueryParams>,
-      headers?: ValidatorSpec<RequestHeaders>
-    },
-    responseSpec?: {
-      data?: ValidatorSpec<ResponseData>,
-      headers?: ValidatorSpec<ResponseHeaders>
-    }
-    handler: (
-      request: Request<RequestParams, RequestData, RequestQueryParams, RequestHeaders>
-    ) => Promise<Response<ResponseData, ResponseHeaders>>
-  }): void {
-
-  }
-
-  get<ResponseData, RequestParams=undefined>(
-    path: Segment<RequestParams>,
-    handler: (request: Request<RequestParams>) => ResponseData
-  ) {
-    return this.route({
-      method: 'GET',
-      path: path,
-      handler: (request) => {
-        const resp = handler(request);
-        return {
-          statusCode: 200,
-          data: handler(request)
-        }
-      }
-    })
-  }
-
-}
-
-const srv = new Server();
-
-srv.route({
-  method: 'GET',
-  path: root._('/')._('username', stringField()),
-  handler: (request) => ({
-    data: {
-      username: request.pathParams.username
-    }
-  })
-})
