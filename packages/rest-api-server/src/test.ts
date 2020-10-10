d3b7efa80708197e5212fc77671124ba3e276462
Anton Berezin <gurunars@gmail.com>
2020-10-10 13:00:03 +0200

    ff

--- packages/rest-api-server/src/test.ts
+++ packages/rest-api-server/src/test.ts
@@ -1,10 +1,10 @@
 import { numberField, objectField, stringField } from '@validator/validator/fields';
 import { root } from '@validator/validator/segmentChain';
-import { GET, serve } from './server';
+import { ANY_METHOD, GET, serve } from './server';
 
 serve({}, [
   GET({
-    pathSpec: root._('/foo')._('username', stringField()),
+    pathSpec: root._('/foo/')._('username', stringField()),
     responseSpec: {
       data: {
         value: stringField(),
@@ -17,7 +17,7 @@ serve({}, [
     })
   }),
   GET({
-    pathSpec: root._('/bla')._('username', stringField()),
+    pathSpec: root._('/bla/')._('username', stringField()),
     responseSpec: {
       data: {
         value: stringField(),
@@ -26,6 +26,19 @@ serve({}, [
     handler: async (request) => ({
       data: {
         value: 'bla ' + request.pathParams.username,
+      },
+    })
+  }),
+  ANY_METHOD({
+    pathSpec: root._('anyPath', stringField()),
+    responseSpec: {
+      data: {
+        message: stringField()
+      }
+    },
+    handler: async () => ({
+      data: {
+        message: 'NOT FOUND',
       },
     })
   })
