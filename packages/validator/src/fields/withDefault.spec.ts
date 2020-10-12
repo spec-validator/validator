7cdac7b9f55c380c42f8b77883a97141d81625cf
Anton Berezin <gurunars@gmail.com>
2020-10-12 09:08:43 +0200

    ff

---
+++ packages/validator/src/fields/withDefault.spec.ts
@@ -0,0 +1,26 @@
+import withDefault from './withDefault'
+import { TypeHint, validate } from '../core'
+import { expectType } from '../TypeTestUtils.test'
+import numberField from './numberField'
+
+const field = withDefault(numberField(), 42)
+
+describe('field', () => {
+  it('should allow undefined value go through', () => {
+    const value = validate(field, undefined)
+    expect(value).toEqual(42)
+  })
+
+  it('should allow undefined nested value go through', () => {
+    const value = validate({
+      inner: field
+    }, {})
+    expect(value.inner).toEqual(42)
+  })
+})
+
+test('types', () => {
+  type Spec = TypeHint<typeof field>
+
+  expectType<number>(42 as Spec)
+})
