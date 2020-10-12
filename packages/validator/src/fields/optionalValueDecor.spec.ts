24689bc123519ad817004dcca44605bfa3622266
Anton Berezin <gurunars@gmail.com>
2020-10-12 08:59:59 +0200

    ff

--- packages/validator/src/fields/optionalValueDecor.spec.ts
+++ packages/validator/src/fields/optionalValueDecor.spec.ts
@@ -1,5 +1,26 @@
-import optionalValueDecor from './optionalValueDecor'
+import optional from './optionalValueDecor'
+import numberField from './numberField'
+import { TypeHint, validate } from '../core'
+import { expectType } from '../TypeTestUtils.test'
+
+const field = optional(numberField())
 
 describe('field', () => {
+  it('should allow undefined value go through', () => {
+    const value = validate(field, undefined)
+    expect(value).toEqual(undefined)
+  })
 
+  it('should allow undefined nested value go through', () => {
+    const value = validate({
+      inner: field
+    }, {})
+    expect(value.inner).toEqual(undefined)
+  })
+})
+
+test('types', () => {
+  type Spec = TypeHint<typeof field>
+
+  expectType<number | undefined>(42 as Spec)
 })
