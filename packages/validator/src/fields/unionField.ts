a23dfb9ccbf46e14758d1f539049f3c8c7901ce3
Anton Berezin <gurunars@gmail.com>
2020-10-17 14:17:55 +0200

    ff

--- packages/validator/src/fields/unionField.ts
+++ packages/validator/src/fields/unionField.ts
@@ -1,73 +1,43 @@
-import { Field, SpecUnion, TypeHint } from '../core'
-import { Json } from '../Json'
-import { merge } from '../utils'
-import booleanField from './booleanField'
-import choiceField from './choiceField'
-import objectField from './objectField'
-import stringField from './stringField'
-
-
-type Unioned<T extends SpecUnion<any>[]> = {
-  [P in keyof T]: T[P] extends SpecUnion<any> ? TypeHint<T[P]> : never
-};
-
-const bla = <T extends SpecUnion<any>[]>(alts: T): Unioned<T> => null as any
-
-const ff = bla([stringField(), choiceField([1, 2, 3] as const), booleanField(), objectField({foo: stringField()})])
-
-
-/*
-
-import { Field } from '../core'
-import { WithRegExp } from '../WithStringInputSupport'
-import { escapeRegex } from '../utils'
+import { Field, SpecUnion, TypeHint, getParams } from '../core'
 import { Primitive, Json } from '../Json'
 
 type Unioned<T extends SpecUnion<any>[]> = {
   [P in keyof T]: T[P] extends SpecUnion<any> ? TypeHint<T[P]> : never
-};
+}[number];
 
-type Params<Variants extends SpecUnion<any>[]> = {
+type Params<Variants extends readonly SpecUnion<any>[]> = {
   readonly variants: Variants,
   readonly description?: string
 }
 
-class ChoiceField<
+class UnionField<
   Variants extends SpecUnion<any>[]
 > implements Field<Variants> {
-  private choicesSet: Set<Primitive>
 
-  constructor(readonly params: Params<readonly Choice[]>) {
-    this.choicesSet = new Set(params.choices)
-  }
-  getFieldWithRegExp(): Field<unknown> & WithRegExp {
-    return new ChoiceFieldWithRegExp(this.params)
-  }
+  constructor(readonly params: Params<Variants>) {}
 
   validate(value: any): Unioned<Variants> {
-    if (this.choicesSet.has(value)) {
-      return value as Choice
-    }
     throw 'Invalid choice'
   }
-  serialize(deserialized: Choice): Json {
+  serialize(deserialized: Variants): Json {
     return deserialized as unknown as Primitive
   }
   getParams() {
-    return this.params
+    return {
+      description: this.params.description,
+      innerSpecs: this.params.variants.map(getParams)
+    }
   }
 
 }
 
 
 const choiceField = <
-  Choice extends Primitive,
-> (choices: readonly Choice[], description?: string): ChoiceField<Choice> =>
-    new ChoiceField({
-      choices,
+  Variants extends SpecUnion<any>[],
+> (variants: Variants, description?: string): UnionField<Variants> =>
+    new UnionField({
+      variants,
       description
     })
 
 export default choiceField
-
-*/
