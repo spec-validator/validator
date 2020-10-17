70d6e8d0bef0ef3f0f8a48a840887c0ba5e3c816
Anton Berezin <gurunars@gmail.com>
2020-10-17 12:10:17 +0200

    ff

--- packages/validator/src/fields/unionField.ts
+++ packages/validator/src/fields/unionField.ts
@@ -1,15 +1,18 @@
-import { Field } from '../core'
+import { Field, SpecUnion, TypeHint } from '../core'
 import { Json } from '../Json'
 import { merge } from '../utils'
 import booleanField from './booleanField'
+import choiceField from './choiceField'
 import objectField from './objectField'
 import stringField from './stringField'
 
 
-type Unioned<T extends Field<unknown>[]> = {
-  [P in keyof T]: T[P] extends Field<unknown> ? ReturnType<T[P]['validate']> : never
+type Unioned<T extends SpecUnion<any>[]> = {
+  [P in keyof T]: T[P] extends SpecUnion<any> ? TypeHint<T[P]> : never
 };
 
-const bla = <T extends Field<unknown>[]>(alts: T): Unioned<T> => null as any
+const bla = <T extends Field<any>[]>(alts: T): Unioned<T> => null as any
 
-const ff = bla([stringField(), objectField({ foo: stringField()}), booleanField()])
+const choices = choiceField([1, 2, 3])
+
+const ff = bla([stringField(), choices, booleanField()])
