1dcee07d2f1974963bacd4d6077b05deaf5625ef
Anton Berezin <gurunars@gmail.com>
2020-10-18 15:00:21 +0200

    ff

--- packages/validator/src/fields/unionField.spec.ts
+++ packages/validator/src/fields/unionField.spec.ts
@@ -1,5 +1,5 @@
 import unionField from './unionField'
-import { TypeHint, validate } from '../core'
+import { getParams, serialize, TypeHint, validate } from '../core'
 import { expectType } from '../TypeTestUtils.test'
 import booleanField from './booleanField'
 import choiceField from './choiceField'
@@ -8,16 +8,38 @@ import stringField from './stringField'
 const choices = choiceField([1, 2, 3])
 
 const field = unionField(
-  [stringField(), choices, booleanField()],
+  [
+    stringField({
+      description: 'Title'
+    }),
+    choices,
+    booleanField({
+      description: 'Flag'
+    })
+  ],
 )
 
-describe('field', () => {
-  it('should allow undefined value go through', () => {
-    expect(validate(field, 1)).toEqual(1)
-    expect(validate(field, 3)).toEqual(3)
-    expect(validate(field, true)).toEqual(true)
-  })
+test('validation', () => {
+  expect(validate(field, 1)).toEqual(1)
+  expect(validate(field, 3)).toEqual(3)
+  expect(validate(field, true)).toEqual(true)
+  expect(validate(field, 'foo')).toEqual('foo')
+})
 
+test('serialization', () => {
+  expect(serialize(field, 1)).toEqual(1)
+  expect(serialize(field, 3)).toEqual(3)
+  expect(serialize(field, true)).toEqual(true)
+  expect(serialize(field, 'foo')).toEqual('foo')
+})
+
+test('getParams', () => {
+  expect(getParams(field)).toEqual({
+    'innerSpecs':[
+      {'description':'Title'},
+      {'choices':[1,2,3]},
+      {'description':'Flag'}
+    ]})
 })
 
 test('types', () => {
