960c3425faf5bd2d78cdb1ca4ec91b3cc885d599
Anton Berezin <gurunars@gmail.com>
2020-11-13 00:31:19 +0100

    ff

--- packages/validator/src/TypeTestUtils.test.ts
+++ packages/validator/src/TypeTestUtils.test.ts
@@ -1,4 +1,3 @@
 export const expectType = <A, B>(
-  // eslint-disable-next-line unused-imports/no-unused-vars-ts
-  value: A extends B ? B extends A ? true : false : false
+  _: A extends B ? B extends A ? true : false : false
 ): void => undefined
