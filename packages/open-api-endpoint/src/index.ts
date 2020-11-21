f626696d8ab986eeac26cba786c23c3ce062fc33
Anton Berezin <gurunars@gmail.com>
2020-11-21 13:33:59 +0100

    ff

--- packages/open-api-endpoint/src/index.ts
+++ packages/open-api-endpoint/src/index.ts
@@ -1,5 +1,4 @@
-import { Route } from '@validator/rest-api-server/'
-import { ServerConfig } from '@validator/rest-api-server/'
+import { ServerConfig, Route } from '@validator/rest-api-server'
 
 const createOpenApiRoute = (
   config: Partial<ServerConfig>,
