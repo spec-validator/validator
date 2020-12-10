150c244af6226c9d11fd0abefae0bf3c501c82c8
Anton Berezin <gurunars@gmail.com>
2020-12-10 23:08:11 +0100

    ff

--- packages/open-api-endpoint/src/withDescription.spec.ts
+++ packages/open-api-endpoint/src/withDescription.spec.ts
@@ -1,4 +1,4 @@
-import { validate, TypeHint, getParams } from '@validator/validator'
+import { TypeHint } from '@validator/validator'
 import { numberField } from '@validator/validator/fields'
 import { expectType } from '@validator/validator/TypeTestUtils.test'
 
@@ -8,18 +8,15 @@ const field = withDescription(numberField(), 'Some description')
 
 describe('field', () => {
   it('should proxy the value to the inner field', () => {
-    const value = validate(field, 42)
-    expect(value).toEqual(42)
+    expect(field.validate(42)).toEqual(42)
   })
 
   it('should annotate params with a description', () => {
-    const value = getParams(field)
-    expect(value).toEqual({'description': 'Some description'})
+    expect(field.getParams()).toEqual({'description': 'Some description'})
   })
 })
 
 test('types', () => {
   type Spec = TypeHint<typeof field>
-
   expectType<Spec, number>(true)
 })
