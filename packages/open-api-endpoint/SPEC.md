cc01266ba1a000149b5dc18a29d58c4a70a5c8c3
Anton Berezin <gurunars@gmail.com>
2020-12-13 01:23:16 +0100

    ff

--- packages/open-api-endpoint/SPEC.md
+++
@@ -1,5 +0,0 @@
-# SPEC
-
-## Data types
-
-
--- packages/open-api-endpoint/src/schemaRegistry.ts
+++ packages/open-api-endpoint/src/schemaRegistry.ts
@@ -1,11 +1,19 @@
 import { Field } from '@validator/validator'
 
 type Schema = {
-  type: 'string' | 'number' | 'integer' | 'boolean' | 'array' | 'object',
+  title?: string, // name of the field
+  type?: 'string' | 'number' | 'integer' | 'boolean' | 'array' | 'object',
   format?: string, // for any string based field
-  pattern?: string,
-  default?: unknown
-  oneOf?: unknown[] // for union field
+  pattern?: string, // for regex in string fields
+  default?: unknown,
+  oneOf?: unknown[], // for union field (excludes type)
+  enum?: unknown[], // for choice field (for multitype choices use oneOf + type + enum since enums must be of type)
+  // array type
+  items?: Schema, // for inner items' schema
+  // object type
+  properties?: Record<string, Schema>, // for type object
+  required?: string[], // for type object (required is an object level field not a property field)
+  additionalProperties?: boolean // if extra, unspecified keys are allowed
 }
 
 type WithType = {
