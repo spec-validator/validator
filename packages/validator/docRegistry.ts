ae22990fa125964b9673b012f6c5da2df6913df0
Anton Berezin <gurunars@gmail.com>
2020-12-18 00:55:33 +0100

    ff

--- packages/validator/docRegistry.ts
+++ packages/validator/docRegistry.ts
@@ -1,7 +1,20 @@
 import { Field, Json } from './src'
+import { SpecUnion } from './src/core'
+import { Any } from './src/util-types'
 
-const generateSchema = <FieldType extends Field<unknown>>(
-  mapping: Map<new () => FieldType, (field: FieldType) => Json>
-) => {
-  // TODO
+type FieldPair<FieldType extends Field<unknown> = Field<unknown>> =
+  [(() => FieldType) & { type: symbol }, (field: FieldType) => Json]
+
+type Registy = [FieldPair]
+
+const getSchema = (
+  ...registries: Registy[]
+) => (specs: SpecUnion<Any>) => {
+  const mapping: Record<symbol, (field: Field<unknown>) => Json> = {}
+  registries.forEach(registry => {
+    registry.forEach(([key, value]) => {
+      mapping[key.type] = value
+    })
+  })
+
 }
