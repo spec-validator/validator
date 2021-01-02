6a0bf41f0515a24f879ee082a4d6cae688c10dd6
Anton Berezin <gurunars@gmail.com>
2021-01-02 21:42:44 +0100

    ff

--- packages/rest-api-server/src/route.spec.ts
+++ packages/rest-api-server/src/route.spec.ts
@@ -1,13 +1,12 @@
 import { choiceField, objectField, stringField } from '@validator/validator/fields'
 import $ from '@validator/validator/fields/segmentField'
 
-import { TypeHint, ValidatorSpec } from '@validator/validator/core'
 import { expectType } from '@validator/validator/TypeTestUtils.test'
-import { Request, Response, Handler, Route } from './route'
+import { Route } from './route'
 
 describe('Route', () => {
 
-  it('works with defined request and response', () => {
+  it('always contains the fields that are defined', () => {
     const reqSpec = {
       method: choiceField('GET'),
       headers: objectField({
@@ -37,82 +36,26 @@ describe('Route', () => {
         }>(true)
   })
 
-  describe('Request', () => {
+  it('always contains a method in a request and statusCode in a response', () => {
+    const reqSpec = {
+      method: choiceField('GET'),
+    }
 
-    it('contains nothing by default', () => {
-      type Req = TypeHint<ValidatorSpec<Request>>
+    const respSpec = {
+      statusCode: choiceField(201),
+    }
 
-      expectType<Req, {
-        method: string
-      }>(true)
-    })
+    type Decor = Route<typeof reqSpec, typeof respSpec>
 
-    it('always contains the fields that are defined', () => {
-      type Req = Request<'GET', {pathKey: string}, undefined, undefined, {headerKey: string}>
-
-      expectType<{
+    expectType<Decor, {
+      request: typeof reqSpec,
+      response: typeof respSpec,
+      handler: (request: {
         method: 'GET',
-        headers: { headerKey: string },
-        pathParams: { pathKey: string },
-      }, Req>(true)
-    })
-
-  })
-
-  describe('Response', () => {
-
-    it('contains nothing by default', () => {
-      type Resp = Response
-
-      expectType<Resp, {
-        statusCode: number
-      }>(true)
-    })
-
-    it('always contains the fields that are defined', () => {
-      type Resp = Response<201 | 404, string>
-
-      expectType<{
-        statusCode: 201 | 404,
-        data: string
-      }, Resp>(true)
-    })
-
-  })
-
-  describe('Handler', () => {
-
-    it('in request: always contains a method', () => {
-      type H = Handler
-      expectType<H, ((request: {
-        method: string
       }) => Promise<{
-        statusCode: number
-      }>)>(true)
-    })
-
-    it('in request: always contains the fields that are defined', () => {
-      type Req = Request<'GET', {pathKey: string}, undefined, undefined, {headerKey: string}>
-      type H = Handler<Req>
-      expectType<H, ((request: {
-        method: 'GET',
-        headers: { headerKey: string },
-        pathParams: { pathKey: string },
-      }) => Promise<{
-        statusCode: number
-      }>)>(true)
-    })
-
-    it('in response: always contains the fields that are defined', () => {
-      type H = Handler<Request, Response<201 | 404, string>>
-      expectType<H, ((request: {
-        method: string
-      }) => Promise<{
-        statusCode: 201 | 404,
-        data: string
-      }>)>(true)
-    })
-
+        statusCode: 201,
+      }>
+        }>(true)
   })
 
 })
