38ec853a2e9b27209df7bab26d60cbaafe354d1c
Anton Berezin <gurunars@gmail.com>
2021-01-03 17:01:53 +0100

    Object schema done

--- packages/open-api-endpoint/src/index.ts
+++ packages/open-api-endpoint/src/index.ts
@@ -2,10 +2,6 @@ import { OpenAPIV3 as OpenAPI } from 'openapi-types'
 
 import { ServerConfig, Route } from '@validator/rest-api-server'
 import { Field } from '@validator/validator'
-import { $, objectField } from '@validator/validator/fields'
-import { StringMapping } from '@validator/rest-api-server/route'
-
-// https://swagger.io/specification/
 
 const createParameter = (type: 'query' | 'path', name: string, field: Field<unknown>): OpenAPI.ParameterObject => ({
   name: name,
@@ -17,16 +13,6 @@ const createParameter = (type: 'query' | 'path', name: string, field: Field<unkn
   }
 })
 
-type PathSpec<PathParams extends StringMapping | unknown = StringMapping | unknown> = typeof $ & Field<PathParams>
-
-const createRoute = (segment: PathSpec): string => segment.toString()
-
-const createPathParams = (segment: PathSpec): Record<string, Field<unknown>> => ({})
-
-type QuerySpec<QueryParams extends StringMapping = StringMapping> = ReturnType<typeof objectField> & Field<QueryParams>
-
-const createQueryParams = (params?: QuerySpec): Record<string, Field<unknown>> => params?.objectSpec || {}
-
 const specToParams = (
   type: 'query' | 'path',
   spec?: Record<string, Field<unknown>>
@@ -35,11 +21,11 @@ const specToParams = (
     ([name, field]) => createParameter(type, name, field)
   )
 
-const createPath = (route: Route): [string, OpenAPI.PathItemObject] => [createRoute(route.request.pathParams), {
+const createPath = (route: Route): [string, OpenAPI.PathItemObject] => [route.request.pathParams.toString(), {
   [(route.request.method.constant as string).toLowerCase()]: {
     parameters: [
-      ...specToParams('query', createQueryParams(route.request.queryParams)),
-      ...specToParams('path', createPathParams(route.request.pathParams))
+      ...specToParams('query', route.request.queryParams?.objectSpec || {}),
+      ...specToParams('path', route.request.pathParams.getObjectSpec())
     ],
     requestBody: {
       // TODO: inject media type from server configs
