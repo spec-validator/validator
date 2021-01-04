f9e4bec176938f351bf7d55821e481e386c95433
Anton Berezin <gurunars@gmail.com>
2021-01-04 21:50:30 +0100

    ff

--- packages/validator/src/fields/stringField.ts
+++ packages/validator/src/fields/stringField.ts
@@ -1,37 +1,29 @@
-import { declareField } from '../registry'
-import { Json } from '../Json'
+import { field, OfType } from '../registry'
 import { FieldWithStringInputSupport, FieldWithRegExp } from './segmentField'
 
-class StringField implements FieldWithStringInputSupport<string>, FieldWithRegExp<string> {
-  private regexV?: RegExp;
+export interface StringField extends FieldWithStringInputSupport<string>, FieldWithRegExp<string> {}
 
-  constructor(regex?: RegExp) {
-    this.regexV = regex
-  }
-  getFieldWithRegExp(): StringField {
-    return this
-  }
-  asString(value: string) {
-    return value
-  }
-  get regex() {
-    return this?.regexV || /.*/
-  }
-  validate(value: any): string {
-    if (typeof value !== 'string') {
-      throw 'Not a string'
-    }
-    if (this.regexV) {
-      const match = value.match(this.regexV)
-      if (!match) {
-        throw 'Doesn\'t match a regex'
+export default field('@validator/fields.StringField', (
+  regex?: RegExp
+): StringField => {
+  const field = {
+    regex: regex || /.*/,
+    validate: (value: any): string => {
+      if (typeof value !== 'string') {
+        throw 'Not a string'
       }
-    }
-    return value
-  }
-  serialize(deserialized: string): Json {
-    return deserialized
-  }
-}
+      if (regex) {
+        const match = value.match(regex)
+        if (!match) {
+          throw 'Doesn\'t match a regex'
+        }
+      }
+      return value
+    },
+    serialize: (deserialized: string) => deserialized
+  } as StringField & OfType<string>
 
-export default declareField('@validator/fields.StringField', StringField)
+  field.getFieldWithRegExp = () => field
+
+  return field
+})
