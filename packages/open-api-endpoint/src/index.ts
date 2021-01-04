3412941b0f45f68039269acfc7f8bd47b973fc70
Anton Berezin <gurunars@gmail.com>
2021-01-04 02:50:57 +0100

    ff

--- packages/open-api-endpoint/src/index.ts
+++ packages/open-api-endpoint/src/index.ts
@@ -6,66 +6,6 @@ import { GetRepresentation, OfType } from '@validator/validator/registry'
 
 import getFieldSchema from './schemaRegistry'
 import { optional } from '@validator/validator/fields'
-
-const createParameter = (
-  getSchema: GetRepresentation,
-  type: 'query' | 'path',
-  name: string,
-  field: Field<unknown>
-): OpenAPI.ParameterObject => ({
-  name: name,
-  in: type,
-  description: (field as any)?.description,
-  required: !(field as any)?.isOptional,
-  schema: getSchema(field)
-})
-
-const specToParams = (
-  getSchema: GetRepresentation,
-  type: 'query' | 'path',
-  spec?: Record<string, Field<unknown>>
-): OpenAPI.ParameterObject[] =>
-  Object.entries(spec || {}).map(
-    ([name, field]) => createParameter(getSchema, type, name, field)
-  )
-
-const createOperationObject = (getSchema: GetRepresentation, route: Route): OpenAPI.OperationObject => ({
-  parameters: [
-    ...specToParams(getSchema, 'query', route.request.queryParams?.objectSpec || {}),
-    ...specToParams(getSchema, 'path', route.request.pathParams.getObjectSpec())
-  ],
-  requestBody: route.request.data && {
-    // TODO: inject media type from server configs
-    content: { 'application/json': {
-      schema: getSchema(route.request.data)
-    } },
-    // TODO: extra test is required
-    required: (route.request.data as unknown as OfType<string>).type !== optional.type
-  },
-  responses: {
-    code: {
-      headers: { 'header-name': 'header-value' },
-      content: { 'application/json': {
-        schema: {
-
-        }
-      } }
-    }
-  }
-})
-
-const createPath = (getSchema: GetRepresentation, route: Route):
-  [string, OpenAPI.PathItemObject] => [route.request.pathParams.toString(), {
-  [(route.request.method.constant as string).toLowerCase()]: createOperationObject(getSchema, route)
-}]
-
-type WithInfo = {
-  info: {
-    title: string,
-    version: string
-  }
-}
-
 const mergeValues = (pairs: [a: string, b: OpenAPI.PathItemObject][]): Record<string, OpenAPI.PathItemObject> => {
   const record: Record<string, OpenAPI.PathItemObject>  = {}
   pairs.forEach(([a, b]) => {
@@ -77,19 +17,88 @@ const mergeValues = (pairs: [a: string, b: OpenAPI.PathItemObject][]): Record<st
   return record
 }
 
-const createOpenApiSpec = (
-  config: ServerConfig & WithInfo,
-  routes: Route[],
-  getSchema: GetRepresentation = getFieldSchema
-): OpenAPI.Document => ({
-  openapi: '3.0.3',
-  info: config.info,
-  servers: [
-    {
-      url: config.baseUrl
-    }
-  ],
-  paths: mergeValues(routes.map(createPath.bind(null, getSchema, config.serialization)))
-})
+type WithInfo = {
+  info: {
+    title: string,
+    version: string
+  }
+}
 
-export default createOpenApiSpec
+class OpenApiGenerator {
+
+  constructor(
+    readonly config: ServerConfig & WithInfo,
+    readonly routes: Route[],
+    readonly getSchema: GetRepresentation = getFieldSchema
+  ) {}
+
+  createOpenApiSpec(): OpenAPI.Document {
+    return {
+      openapi: '3.0.3',
+      info: this.config.info,
+      servers: [
+        {
+          url: this.config.baseUrl
+        }
+      ],
+      paths: mergeValues(this.routes.map(this.createPath))
+    }
+  }
+
+  createParameter(
+    type: 'query' | 'path',
+    name: string,
+    field: Field<unknown>
+  ): OpenAPI.ParameterObject {
+    return {
+      name: name,
+      in: type,
+      description: (field as any)?.description,
+      required: !(field as any)?.isOptional,
+      schema: this.getSchema(field)
+    }
+  }
+
+  specToParams(
+    type: 'query' | 'path',
+    spec?: Record<string, Field<unknown>>
+  ): OpenAPI.ParameterObject[] {
+    return Object.entries(spec || {}).map(
+      ([name, field]) => this.createParameter(type, name, field)
+    )
+  }
+
+  createOperationObject(route: Route): OpenAPI.OperationObject {
+    return {
+      parameters: [
+        ...this.specToParams('query', route.request.queryParams?.objectSpec || {}),
+        ...this.specToParams('path', route.request.pathParams.getObjectSpec())
+      ],
+      requestBody: route.request.data && {
+      // TODO: inject media type from server configs
+        content: { 'application/json': {
+          schema: this.getSchema(route.request.data)
+        } },
+        // TODO: extra test is required
+        required: (route.request.data as unknown as OfType<string>).type !== optional.type
+      },
+      responses: {
+        code: {
+          headers: { 'header-name': 'header-value' },
+          content: { 'application/json': {
+            schema: {
+
+            }
+          } }
+        }
+      }
+    }
+  }
+
+  createPath(route: Route): [string, OpenAPI.PathItemObject] {
+    return [route.request.pathParams.toString(), {
+      [(route.request.method.constant as string).toLowerCase()]: this.createOperationObject(route)
+    }]
+  }
+
+}
