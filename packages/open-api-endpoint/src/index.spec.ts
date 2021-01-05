ff740d34239eafad88e618857a1b0c137dd2e0fc
Anton Berezin <gurunars@gmail.com>
2021-01-05 01:01:35 +0100

    ff

--- packages/open-api-endpoint/src/index.spec.ts
+++ packages/open-api-endpoint/src/index.spec.ts
@@ -1,23 +1,47 @@
 import { GET, Route } from '@validator/rest-api-server'
-import { $, stringField, objectField, constantField, booleanField } from '@validator/validator/fields'
+import {
+  $, stringField, objectField, constantField, booleanField, numberField, arrayField
+} from '@validator/validator/fields'
 import genOpenApi from '.'
 import { DEFAULT_SERVER_CONFIG } from '@validator/rest-api-server/server'
+import withDoc from './withDoc'
 
 test('fullRoute', () => {
   const routes: Route[] = [
     GET($._('/item/')._('id', stringField()), {
       request: {
+        headers: objectField({
+          key: withDoc(numberField(), {
+            description: 'key header',
+            examples: {
+              sampleKey: {
+                value: 13 as number,
+                summary: 'Sample value'
+              }
+            }
+          })
+        }),
         queryParams: objectField({
-          full: booleanField()
+          flag: booleanField(),
+        }),
+        data: objectField({
+          title: stringField(/.*/),
+          count: numberField()
         })
       },
       response: {
         // TODO static field for only one possible value
-        statusCode: constantField(200)
+        statusCode: constantField(200),
+        data: objectField({
+          items: arrayField(numberField())
+        })
       },
     },
     async () => Promise.resolve({
-      statusCode: 200 as const
+      statusCode: 200 as const,
+      data: {
+        items: [1, 2, 3]
+      }
     }))
   ]
 
