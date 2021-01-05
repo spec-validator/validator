9b0d9509168e38fadde42d789cd9e04391f2a364
Anton Berezin <gurunars@gmail.com>
2021-01-05 01:10:15 +0100

    ff

--- packages/open-api-endpoint/src/index.spec.ts
+++ packages/open-api-endpoint/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { GET, Route } from '@validator/rest-api-server'
+import { GET, POST, Route } from '@validator/rest-api-server'
 import {
   $, stringField, objectField, constantField, booleanField, numberField, arrayField
 } from '@validator/validator/fields'
@@ -8,41 +8,54 @@ import withDoc from './withDoc'
 
 test('fullRoute', () => {
   const routes: Route[] = [
-    GET($._('/item/')._('id', stringField()), {
-      request: {
-        headers: objectField({
-          key: withDoc(numberField(), {
-            description: 'key header',
-            examples: {
-              sampleKey: {
-                value: 13 as number,
-                summary: 'Sample value'
-              }
-            }
+    POST($._('/items'),
+      {
+        request: {
+          data: objectField({
+            title: stringField(/.*/),
+            count: numberField()
           })
-        }),
-        queryParams: objectField({
-          flag: booleanField(),
-        }),
-        data: objectField({
-          title: stringField(/.*/),
-          count: numberField()
-        })
+        },
+        response: {
+          statusCode: constantField(201)
+        }
       },
-      response: {
-        // TODO static field for only one possible value
-        statusCode: constantField(200),
-        data: objectField({
-          items: arrayField(numberField())
-        })
+      async () => Promise.resolve({
+        statusCode: 201 as const,
+      })
+    ),
+    GET($._('/item/')._('id', stringField()),
+      {
+        request: {
+          headers: objectField({
+            key: withDoc(numberField(), {
+              description: 'key header',
+              examples: {
+                sampleKey: {
+                  value: 13 as number,
+                  summary: 'Sample value'
+                }
+              }
+            })
+          }),
+          queryParams: objectField({
+            flag: booleanField(),
+          })
+        },
+        response: {
+          statusCode: constantField(200),
+          data: objectField({
+            items: arrayField(numberField())
+          })
+        },
       },
-    },
-    async () => Promise.resolve({
-      statusCode: 200 as const,
-      data: {
-        items: [1, 2, 3]
-      }
-    }))
+      async () => Promise.resolve({
+        statusCode: 200 as const,
+        data: {
+          items: [1, 2, 3]
+        }
+      })
+    )
   ]
 
   expect(genOpenApi({
