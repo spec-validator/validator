d2d207648ff7114745b1aaae38296b958009de92
Anton Berezin <gurunars@gmail.com>
2021-01-08 00:50:08 +0100

    ff

--- packages/rest-api-server/src/serialization/html.spec.ts
+++ packages/rest-api-server/src/serialization/html.spec.ts
@@ -6,6 +6,10 @@ test('serialize', () => {
   expect(html.serialize('html')).toMatchSnapshot()
 })
 
+test('serialize', () => {
+  expect(html.serialize(undefined)).toMatchSnapshot()
+})
+
 test('deserialize', () => {
   expect(html.deserialize('html')).toMatchSnapshot()
 })
