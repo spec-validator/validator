15846ba8facd2e002e5aaa539e0a1afbbe4bb34c
Anton Berezin <gurunars@gmail.com>
2021-01-08 23:45:36 +0100

    ff

--- packages/validator/src/fields/unionField.ts
+++ packages/validator/src/fields/unionField.ts
@@ -1,22 +1,23 @@
-import { Field, serialize, SpecUnion, TypeHint, validate } from '../core'
+import { serialize, validate } from '..'
+import { Field, SpecUnion, TypeHint } from '../core'
 import { field } from '../registry'
 
-export type Unioned<T extends SpecUnion<any>[]> = {
-  [P in keyof T]: T[P] extends SpecUnion<any> ? TypeHint<T[P]> : never
+export type Unioned<T extends SpecUnion<unknown>[]> = {
+  [P in keyof T]: T[P] extends SpecUnion<unknown> ? TypeHint<T[P]> : never
 }[number];
 
-export interface UnionField<Variants extends Field<any>[]> extends Field<Unioned<Variants>> {
+export interface UnionField<Variants extends SpecUnion<any>[]> extends Field<Unioned<Variants>> {
   readonly variants: Variants
 }
 
-export default field('@validator/fields.UnionField', <Variants extends Field<any>[]> (
+export default field('@validator/fields.UnionField', <Variants extends SpecUnion<unknown>[]> (
   ...variants: Variants
 ): UnionField<Variants> => ({
     variants,
     validate: (value: any): Unioned<Variants> => {
       for (const variant of variants) {
         try {
-          return validate(variant, value)
+          return validate(variant, value) as Unioned<Variants>
         } catch {
           // SKIP
         }
