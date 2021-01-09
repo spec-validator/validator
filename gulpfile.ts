96df4bde8cf13bb6983459450853b5fcb10ee4ce
Anton Berezin <gurunars@gmail.com>
2021-01-09 23:40:36 +0100

    ff

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,18 +1,14 @@
 import gulp from 'gulp'
 
-import tsGulp from 'gulp-typescript'
-
-
-// yarn add --dev -W gulp-typescript ts-node typescript @types/gulp gulp
-
-//gulp.task('default', function () {
-//return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'))
-//})
 
 
 
 
-// TODO determine build order based on package.json
+
+
+
+
+
 const getProjectsInBuildOrder = (): string[] => [
   'validator',
   'rest-api-server',
@@ -20,8 +16,7 @@ const getProjectsInBuildOrder = (): string[] => [
 ]
 
 const buildProject = async (name: string) => {
-  const tsProject = tsGulp.createProject(`package/${name}/tsconfig.json`)
-  tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'))
+
 }
 
 export default gulp.series(getProjectsInBuildOrder().map(
