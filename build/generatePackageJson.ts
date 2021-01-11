8c3f1ad3454b6380a06ce10058854d3c14f5da61
Anton Berezin <gurunars@gmail.com>
2021-01-11 21:22:30 +0100

    ff

--- build/generatePackageJson.ts
+++ build/generatePackageJson.ts
@@ -33,9 +33,8 @@ export default (projectPath: string): Task => async () => {
     }
   })
 
-  const version = getOutput('git', 'tag', '--points-at', 'HEAD').toString()
-    .split('\n')
-    .find(it => it.match(/^([0-9]+)\.([0-9]+)\.([0-9]+)$/))
+  const version = getOutput('git', 'describe').toString()
+    .split('\n')[0]
   if (!version) {
     throw 'Commit doesn\'t point at any semver tag - can\'t publish'
   }
