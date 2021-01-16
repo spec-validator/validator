154626a7b6e2308cd9c326b1eb8c194e0aba8519
Anton Berezin <gurunars@gmail.com>
2021-01-16 15:55:35 +0100

    ff

--- packages/doc-tester/src/codeBlocks.ts
+++ packages/doc-tester/src/codeBlocks.ts
@@ -1,50 +1,69 @@
 type CodeBlock = {
   type: string,
   offset: number,
-  snippet: string[]
+  lines: string[]
+}
+
+type CodeBucket = {
+  blocks: CodeBlock[]
 }
 
 const hasDelimiter = (line: string): boolean => line.trim().startsWith('```')
 
-const extractType = (line: string): string =>
-  line.match(/```(?<type>.+)/)?.groups?.type?.trim()?.toLowerCase() || ''
+const extractValue = (regex: RegExp, line: string): string =>
+  line.match(/```(?<value>.+)/)?.groups?.value?.trim()?.toLowerCase() || ''
+
+const extractType = extractValue.bind(null, /```(?<value>.+)/)
+
+const getLabelCheckpoint = extractValue.bind(null, /\w*mark\((?<value>.+)\)\w*/)
+
+const getLabelReset = extractValue.bind(null, /\w*reset\((?<value>.+)\)\w*/)
 
 /**
  * Really primitive FSM based README parser aiming to extract code snippets
  * from the documentation.
  */
-const extractCodeBlocks = (text: string, types: string[]): CodeBlock[] => {
+const extractCodeBlocks = (lines: string[], types: string[]): CodeBlock[] => {
   let block: CodeBlock | undefined = undefined
   const typeSet = new Set(types)
   const result: CodeBlock[] = []
-  text.split('\n').forEach((line, i) => {
-    if (!block) {
-      if (hasDelimiter(line)) {
-        block = {
-          type: extractType(line),
-          offset: i + 1 + 1, // 1 based index + skip the delimiter
-          snippet: [],
-        }
-      } else {
-        // IGNORE
-      }
-    } else {
+
+  // eslint-disable-next-line max-statements
+  lines.forEach((line, i) => {
+    if (block) {
+      //block.label = block.label || getLabelCheckpoint(line)
       if (hasDelimiter(line)) {
         if (typeSet.has(block.type)) {
           result.push(block)
         }
         block = undefined
       } else {
-        block.snippet.push(line)
+        block.lines.push(line)
+      }
+    } else {
+      if (hasDelimiter(line)) {
+        block = {
+          type: extractType(line),
+          offset: i + 1 + 1, // 1 based index + skip the delimiter
+          lines: [],
+        }
+      } else {
+        // IGNORE
       }
     }
   })
   return result
 }
 
+const splitIntoBucets = (blocks: CodeBlock[]): CodeBucket[] => {
+  const buckets: CodeBucket[] = []
+
+  return buckets
+}
+
 const joinCodeBlocks = (snippets: CodeBlock[]): string => snippets
-  .map(it => it.snippet.join('\n'))
+  .map(it => it.lines.join('\n'))
   .join('\n\n')
 
 export default (text: string, types: string[]): string =>
-  joinCodeBlocks(extractCodeBlocks(text, types))
+  joinCodeBlocks(extractCodeBlocks(text.split('\n'), types))
