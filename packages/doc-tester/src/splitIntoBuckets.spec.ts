97828f24e61f35056fce9a3c282b4dbfb8c48a4e
Anton Berezin <gurunars@gmail.com>
2021-01-16 20:33:04 +0100

    ff

--- packages/doc-tester/src/splitIntoBuckets.spec.ts
+++ packages/doc-tester/src/splitIntoBuckets.spec.ts
@@ -62,3 +62,60 @@ test('typed and labeled blocks', () => {
     ['ts']
   )).toMatchSnapshot()
 })
+
+
+test('typed and unlabeled blocks', () => {
+  expect(splitIntoBuckets(
+    [
+      '# Title',
+      '```ts',
+      'CODE',
+      '```',
+      'Text',
+      '```ts',
+      'CODE 2',
+      '```',
+    ],
+    ['ts']
+  )).toMatchSnapshot()
+})
+
+test('untyped and labeled blocks', () => {
+  expect(splitIntoBuckets(
+    [
+      '# Title',
+      '``` one',
+      'CODE',
+      '```',
+      'Text',
+      '``` one',
+      'CODE 2',
+      '```',
+    ],
+    ['']
+  )).toMatchSnapshot()
+})
+
+test('mixed blocks', () => {
+  expect(splitIntoBuckets(
+    [
+      '#Title 1',
+      '```ts one',
+      'Labeled ts code',
+      '```',
+      '#Title 2',
+      '```ts',
+      'Unlabeled ts code',
+      '```',
+      '#Title 3',
+      '``` one',
+      'Labeled wildcard code',
+      '```',
+      '#Title 3',
+      '```',
+      'Unlabeled wildcard code',
+      '```',
+    ],
+    ['ts', '']
+  )).toMatchSnapshot()
+})
