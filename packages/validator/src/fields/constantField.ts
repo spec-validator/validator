c2e12f4dc2776a1c2a22c6db42ead3e09a7ddd46
Anton Berezin <gurunars@gmail.com>
2021-01-20 22:46:33 +0100

    ff

--- packages/validator/src/fields/constantField.ts
+++ packages/validator/src/fields/constantField.ts
@@ -1,13 +1,18 @@
 import { Primitive } from '@spec-validator/utils/Json'
-import { declareField, Field } from '../core'
+import { escapeRegex } from '@spec-validator/utils/utils'
+import { declareField } from '../core'
+import { FieldWithRegExp, FieldWithStringInputSupport } from './segmentField'
 
-export interface ConstantField<Constant extends Primitive> extends Field<Constant> {
+export interface ConstantField<Constant extends Primitive> extends FieldWithStringInputSupport<Constant> {
   readonly constant: Constant
 }
 
 export default declareField('@spec-validator/validator/fields/constantField', <Constant extends Primitive> (
   constant: Constant
-): ConstantField<Constant> => ({
+): ConstantField<Constant> => {
+
+  const result = ({
+    regexp: escapeRegex(constant.toString()),
     constant,
     validate: (value: any): Constant => {
       if (value !== constant) {
@@ -16,4 +21,8 @@ export default declareField('@spec-validator/validator/fields/constantField', <C
       return constant
     },
     serialize: () => constant,
-  }))
+  }) as unknown as ConstantField<Constant> & FieldWithRegExp<Constant>
+  result.getFieldWithRegExp = () => result
+
+  return result
+})
