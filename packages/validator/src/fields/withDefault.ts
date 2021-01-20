65d2b6fc198dfc261396ee86a32182f07e0a3420
Anton Berezin <gurunars@gmail.com>
2021-01-20 23:48:54 +0100

    ff

--- packages/validator/src/fields/withDefault.ts
+++ packages/validator/src/fields/withDefault.ts
@@ -1,7 +1,6 @@
 import { declareField, Field, SpecUnion } from '../core'
-import { getFieldForSpec } from '../interface'
 import { Any } from '@spec-validator/utils/util-types'
-import { FieldWithRegExp, FieldWithStringInputSupport, isFieldWithStringInputSupport } from './segmentField'
+import withDecor from '../withDecor'
 
 export interface WithDefault<T extends Any> extends Field<T> {
   readonly innerSpec: SpecUnion<T>,
@@ -11,11 +10,7 @@ export interface WithDefault<T extends Any> extends Field<T> {
 export default declareField('@spec-validator/validator/fields/withDefault', <T extends Any, Spec extends SpecUnion<T>> (
   innerSpec: Spec & SpecUnion<T>,
   defaultValue: T
-): Spec extends FieldWithStringInputSupport<T>
-? WithDefault<T> & { getFieldWithRegExp(): FieldWithRegExp<T> }
-: WithDefault<T> => {
-  const innerField = getFieldForSpec(innerSpec) as Field<T>
-  const getRawField = (inner: Field<T>): WithDefault<T> => ({
+) => withDecor(innerSpec, (inner: Field<T>): WithDefault<T> => ({
     innerSpec,
     defaultValue,
     validate: (value: any): T => {
@@ -25,22 +20,4 @@ export default declareField('@spec-validator/validator/fields/withDefault', <T e
       return inner.validate(value)
     },
     serialize: (deserialized: T) => inner.serialize(deserialized),
-  })
-
-  const raw = getRawField(innerField)
-
-  if (isFieldWithStringInputSupport(innerField)) {
-    const withRegex = innerField.getFieldWithRegExp() as FieldWithRegExp<T>
-
-    return {
-      ...raw,
-      getFieldWithRegExp: () => ({
-        ...getRawField(withRegex),
-        regex: withRegex.regex,
-      }),
-    } as any
-
-  } else {
-    return raw as any
-  }
-})
+  })))
