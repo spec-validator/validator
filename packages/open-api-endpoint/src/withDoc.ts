be842e41984bab37c1a9380496a8d038271400dc
Anton Berezin <gurunars@gmail.com>
2021-01-20 23:52:17 +0100

    ff

--- packages/open-api-endpoint/src/withDoc.ts
+++ packages/open-api-endpoint/src/withDoc.ts
@@ -1,9 +1,7 @@
 import { declareField, Field, OfType, SpecUnion } from '@spec-validator/validator/core'
 import { Any } from '@spec-validator/utils/util-types'
-import { getFieldForSpec } from '@spec-validator/validator/interface'
-import {
-  FieldWithStringInputSupport, FieldWithRegExp, isFieldWithStringInputSupport,
-} from '@spec-validator/validator/fields/segmentField'
+
+import withDecor from '@spec-validator/validator/withDecor'
 
 type Example<T extends Any> = {
   value: T,
@@ -27,32 +25,10 @@ export type Placeholder = OfType<'Placeholder'>
 export default declareField('@spec-validator/fields.WithDoc', <T extends Any, Spec extends SpecUnion<T>> (
   innerSpec: Spec & SpecUnion<T>,
   doc: Doc<T>
-): Spec extends FieldWithStringInputSupport<T> ? WithDoc<T> & {
-  getFieldWithRegExp(): FieldWithRegExp<T>
-} : WithDoc<T> => {
-  const innerField = getFieldForSpec(innerSpec) as Field<T>
-
-  const getRawField = (inner: Field<T>): WithDoc<T> => ({
-    innerSpec,
-    doc,
-    validate: (it: any) => inner.validate(it),
-    serialize: (it: T) => inner.serialize(it),
-  }) // as unknown as WithDoc<T> & FieldWithRegExp<T> & FieldWithStringInputSupport<T>
-
-  const raw = getRawField(innerField)
-
-  if (isFieldWithStringInputSupport(innerField)) {
-    const withRegex = innerField.getFieldWithRegExp() as FieldWithRegExp<T>
-
-    return {
-      ...raw,
-      getFieldWithRegExp: () => ({
-        ...getRawField(withRegex),
-        regex: withRegex.regex,
-      }),
-    } as any
-
-  } else {
-    return getRawField(innerField) as any
-  }
-})
+) =>
+    withDecor(innerSpec, (inner: Field<T>): WithDoc<T> => ({
+      innerSpec,
+      doc,
+      validate: (it: any) => inner.validate(it),
+      serialize: (it: T) => inner.serialize(it),
+    })))
