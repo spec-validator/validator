5f83722f6dc5dac68a1df1a1cfc53baccf49d7e9
Anton Berezin <gurunars@gmail.com>
2021-01-23 00:51:21 +0100

    ff

--- packages/rest-api-server/src/fields/stringSpec.ts
+++ packages/rest-api-server/src/fields/stringSpec.ts
@@ -1,8 +1,12 @@
-import { Any } from '@spec-validator/utils/util-types'
+import { Any, Optional } from '@spec-validator/utils/util-types'
 import { FieldWithStringInputSupport } from '@spec-validator/validator/fields/segmentField'
 
 export type StringObjectSpec<DeserializedType extends Record<string, Any> = Record<string, Any>> = {
   [P in keyof DeserializedType]: FieldWithStringInputSupport<DeserializedType[P]>
+}
+
+export type WildcardStringObjectSpec = {
+  [key: string]: Optional<StringSpecUnion<unknown>>
 }
 
 export type StringArraySpec<DeserializedType extends Any[] = Any[]> = StringSpecUnion<DeserializedType[number]>[]
