095147e8a2a2d243872c2eeda9857aafd770eb72
Anton Berezin <gurunars@gmail.com>
2021-02-04 14:40:01 +0100

    ff

--- packages/rest-api-server/src/route.spec.ts
+++ packages/rest-api-server/src/route.spec.ts
@@ -4,32 +4,28 @@ import {
 } from '@spec-validator/validator/fields'
 import { expectType } from '@spec-validator/test-utils/expectType'
 
-import { Route, route } from './route'
+import { route } from './route'
 import { headerObjectField } from './fields'
 
 describe('Route', () => {
 
   it('works with cookies', () => {
-    const reqSpec = {
-      method: constantField('GET'),
-      headers: {
-        cookies: headerObjectField({
-          cookie1: stringField(),
-        }),
+    const rt = route({
+      request: {
+        method: constantField('GET'),
+        headers: {
+          cookies: headerObjectField({
+            cookie1: stringField(),
+          }),
+        },
+        pathParams: $._('pathKey', stringField()),
       },
-      pathParams: $._('pathKey', stringField()),
-    }
+      response: {
+        statusCode: constantField(201),
+      },
+    })
 
-    const respSpec = {
-      statusCode: constantField(201),
-    }
-
-    type Decor = Route<typeof reqSpec, typeof respSpec>
-
-    expectType<Decor, {
-      request: typeof reqSpec,
-      response: typeof respSpec,
-      handler: (request: {
+    expectType<Parameters<typeof rt.handler>[0],  (request: {
         method: 'GET',
         headers: { cookies: {
           cookie1: string
@@ -37,86 +33,72 @@ describe('Route', () => {
         pathParams: { pathKey: string },
       }) => Promise<{
         statusCode: 201,
-      }>
-        }>(true)
+      }>>(true)
   })
 
   it('always contains the fields that are defined', () => {
-    const reqSpec = {
-      method: constantField('GET'),
-      headers: {
-        headerKey: stringField(),
+
+    const rt = route({
+      request: {
+        method: constantField('GET'),
+        headers: {
+          headerKey: stringField(),
+        },
+        pathParams: $._('pathKey', stringField()),
       },
-      pathParams: $._('pathKey', stringField()),
-    }
+      response: {
+        statusCode: constantField(201),
+        body: stringField(),
+      },
+    })
 
-    const respSpec = {
-      statusCode: constantField(201),
-      body: stringField(),
-    }
-
-    type Decor = Route<typeof reqSpec, typeof respSpec>
-
-    expectType<Decor, {
-      request: typeof reqSpec,
-      response: typeof respSpec,
-      handler: (request: {
+    expectType<Parameters<typeof rt.handler>[0], (request: {
         method: 'GET',
         headers: { headerKey: string },
         pathParams: { pathKey: string },
       }) => Promise<{
         statusCode: 201,
         body: string
-      }>
-        }>(true)
+      }>>(true)
   })
 
   it('always contains a method in a request and statusCode in a response', () => {
-    const reqSpec = {
-      method: constantField('GET'),
-      pathParams: $,
-    }
-
-    const respSpec = {
-      statusCode: constantField(201),
-    }
-
-    type Decor = Route<typeof reqSpec, typeof respSpec>
-
-    expectType<Decor, {
-      request: typeof reqSpec,
-      response: typeof respSpec,
-      handler: (request: {
-        method: 'GET',
-      }) => Promise<{
-        statusCode: 201,
-      }>
-        }>(true)
-  })
-
-  it('works with a union of responses', () => {
-    const reqSpec = {
-      method: constantField('GET'),
-      pathParams: $,
-    }
-
-    const respSpec = unionField({
-      statusCode: constantField(201),
-      body: numberField(),
-    }, {
-      statusCode: constantField(202),
-      body: choiceField('one', 'two'),
-      headers: {
-        headerKey: stringField(),
+    const rt = route({
+      request: {
+        method: constantField('GET'),
+        pathParams: $,
+      },
+      response: {
+        statusCode: constantField(201),
       },
     })
 
-    type Decor = Route<typeof reqSpec, typeof respSpec>
+    expectType<Parameters<typeof rt.handler>[0], (request: {
+        method: 'GET',
+      }) => Promise<{
+        statusCode: 201,
+      }>>(true)
+  })
 
-    expectType<Decor, {
-      request: typeof reqSpec,
-      response: typeof respSpec,
-      handler: (request: {
+  it('works with a union of responses', () => {
+    const rt = route({
+      request: {
+        method: constantField('GET'),
+        pathParams: $,
+      },
+      response: unionField({
+        statusCode: constantField(201),
+        body: numberField(),
+      }, {
+        statusCode: constantField(202),
+        body: choiceField('one', 'two'),
+        headers: {
+          headerKey: stringField(),
+        },
+      }),
+    })
+
+    expectType<Parameters<typeof rt.handler>[0], (request: {
         method: 'GET',
       }) => Promise<{
         statusCode: 201,
@@ -127,7 +109,7 @@ describe('Route', () => {
         headers: {
           headerKey: string
         }
-      }>}>(true)
+      }>>(true)
   })
 })
 
