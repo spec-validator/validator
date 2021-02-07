5b2c6269c3d0ca70545497205505fc0b4bb86c12
Anton Berezin <gurunars@gmail.com>
2021-02-07 03:26:57 +0100

    ff

--- packages/open-api-endpoint/src/withOpenApi.spec.ts
+++ packages/open-api-endpoint/src/withOpenApi.spec.ts
@@ -7,9 +7,9 @@ test('routing', () => {
 })
 
 test('schema root handler', async () => {
-  expect(await fullRoute.routes[0]).toMatchSnapshot()
+  expect(await fullRoute.routes[0].handler(undefined as any)).toMatchSnapshot()
 })
 
 test('schema root handler ui', async () => {
-  expect(await fullRoute.routes[1]).toMatchSnapshot()
+  expect(await fullRoute.routes[1].handler(undefined as any)).toMatchSnapshot()
 })
