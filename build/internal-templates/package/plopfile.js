2639c806ccc83a73dff3dfee8ac4610f98c1c56d
Anton Berezin <gurunars@gmail.com>
2021-02-22 01:05:44 +0100

    Added generation of root README and tsconfig entries

--- build/internal-templates/package/plopfile.js
+++ build/internal-templates/package/plopfile.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const item = (path) => ({
   type: 'add',
   path: `../../../packages/{{name}}/${path}`,
@@ -15,6 +17,38 @@ module.exports = function (plop) {
       item('src/index.spec.ts'),
       item('README.md'),
       item('package.json'),
+      (answers) => {
+        const tsConfigPath = `${plop.getPlopfilePath()}/../../../tsconfig.json`
+        const tsConfig = JSON.parse(
+          fs.readFileSync(tsConfigPath).toString()
+        )
+        tsConfig.compilerOptions.paths = {
+          ...tsConfig.compilerOptions.paths,
+          [`@spec-validator/${answers.name}/*`]: [`${answers.name}/src/*`],
+          [`@spec-validator/${answers.name}`]: [`${answers.name}/src/index.ts`],
+        }
+        fs.writeFileSync(
+          tsConfigPath,
+          JSON.stringify(tsConfig, null, 2) + '\n'
+        )
+      },
+      (answers) => {
+        const readMePath = `${plop.getPlopfilePath()}/../../../README.md`
+        const readMe = fs.readFileSync(readMePath).toString()
+        const appendix = [
+          '',
+          `- [@spec-validator/${answers.name}](packages/${answers.name}/README.md)`,
+          '',
+          '  FILL ME',
+          '',
+          `  \`yarn add --dev @spec-validator/${answers.name}\``,
+          '',
+        ]
+        fs.writeFileSync(
+          readMePath,
+          readMe + appendix.join('\n')
+        )
+      },
     ],
   })
 }
