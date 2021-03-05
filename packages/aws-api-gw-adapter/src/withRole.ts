4d077128c69703055589a2ed1cb2f4aab5ac0f69
Anton Berezin <gurunars@gmail.com>
2021-03-05 20:02:26 +0100

    ff

--- packages/aws-api-gw-adapter/src/withRole.ts
+++ packages/aws-api-gw-adapter/src/withRole.ts
@@ -1,44 +1,14 @@
-//import { Route } from '@spec-validator/rest-api-server/route'
+import { Route } from '@spec-validator/rest-api-server/route'
 import { FilterFlags } from '@spec-validator/utils/util-types'
 
-type WithRole<Role extends string> = {
+export type WithRole<Role extends string> = {
   role: Role
 }
 
-/*
 export const withRole = <Role extends string>(route: Route, role: Role): Route & WithRole<Role> => ({
   ...route,
   role,
 })
 
-const hasRole = (item: any): item is WithRole<string> =>
-  typeof item.role === 'string'
-
-export const getRouteRoles = <T extends Route[]> (routes: T): FilterFlags<T, WithRole<string>> =>
-  routes.filter(hasRole)
-
-*/
-
-type Route = {
-  foo: number
-}
-
-const rolesOf = <T extends readonly Route[]>(routes: T): FilterFlags<T, WithRole<string>> =>
+export const rolesOf = <T extends readonly Route[]>(...routes: T): FilterFlags<T, WithRole<string>> =>
   routes.filter(it => (it as any).role) as any
-
-const foo = rolesOf([
-  {
-    foo: 11,
-  },
-  {
-    foo: 12,
-  },
-  {
-    foo: 13,
-    role: 'role 11',
-  },
-  {
-    foo: 14,
-    role: 'role 12',
-  },
-] as const)
