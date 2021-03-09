12d3d74f9cfe754a529eaa42d876cee5292bd210
Anton Berezin <gurunars@gmail.com>
2021-03-09 13:46:31 +0100

    ff

--- packages/aws-api-gw-adapter/src/withAlias.ts
+++ packages/aws-api-gw-adapter/src/withAlias.ts
@@ -1,30 +1,7 @@
-import { StringObjectSpec } from '@spec-validator/rest-api-server/fields/stringSpec'
 import { Route } from '@spec-validator/rest-api-server/route'
-import { Any, FilterFlags, Optional } from '@spec-validator/utils/util-types'
+//import { FilterFlags } from '@spec-validator/utils/util-types'
 import { SegmentField } from '@spec-validator/validator/fields'
 
-export type WithAwsMeta<
-  Alias extends string,
-  DeserializedType extends Optional<Record<string, Any>> = undefined
-> = {
-  awsMeta:
-  DeserializedType extends Record<string, Any> ? {
-    alias: Alias
-    spec: StringObjectSpec<DeserializedType>
-  } : { alias: Alias}
-}
-
-export const withAwsMeta = <Alias extends string, DeserializedType extends Optional<Record<string, Any>> = undefined>(
-  route: Route,
-  awsMeta: WithAwsMeta<Alias, DeserializedType>['awsMeta']
-): Route & WithAwsMeta<Alias, DeserializedType> =>
-    ({
-      ...route,
-      awsMeta,
-    })
-
-export const awsMetasOf = <T extends readonly Route[]>(...routes: T): FilterFlags<T, WithAwsMeta<string>> =>
-  routes.filter(it => (it as any).awsMeta) as any
 
 export const getPathParamsKey = (route: SegmentField<unknown>): string =>
   route.segments.map((it => it.field ? `{${it.key}}` : `${it.key}`)).join('')
