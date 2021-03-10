8c3e9bc4e19c78fae8be997e0bceaca97f1baee2
Anton Berezin <gurunars@gmail.com>
2021-03-10 14:41:56 +0100

    ff

--- just.config.ts
+++ just.config.ts
@@ -3,8 +3,10 @@ import { task } from 'just-task'
 
 import exec from '@spec-validator/private-build-scripts/exec'
 
-// eslint-disable-next-line unused-imports/no-unused-imports-ts
 import * as doJustTaskDefinitions from '@spec-validator/private-build-scripts/justConfig'
+import assert from 'assert'
+
+assert(doJustTaskDefinitions)
 
 const tsNode = (script: string, isSrv=false) =>
   ['yarn', isSrv ? 'ts-node-dev' : 'ts-node', '-r', 'tsconfig-paths/register', script]
